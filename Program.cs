861164273480a8200b170edfa2db23d5ef8d68ba
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-09-06 20:13:13 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace array_prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[100];
+            int temp;
+            Console.WriteLine("Enter size of array");
+            int s = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter {0} elements",s);
+            for (int i = 0; i < s; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                for (int j = i+1; j < s; j++)
+                {
+                      if(arr[i]>arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("ascending order");
+            for (int i = 0; i < s; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Maxi_min
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            int[] array ={10, 5, 20, 30, 60, 1};
+            int max,min;
+            max = array[0];
+            min = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                 if(array[i]>max)
+                {
+                    max=array[i];
+                }
+                 if(array[i]<min)
+                {
+                     min = array[i];
+                }
+            }                  
+                    Console.WriteLine("Max value is:"+max);
+                    Console.WriteLine("Min value is:"+min);
+        }
+    }
+}
